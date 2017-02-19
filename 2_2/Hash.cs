90445d6feee6d3ea2a3af81a36a0d2160ae80cec
justprosh <just.prosh@gmail.com>
2017-02-19 20:35:10 +0300

    HW

---
+++ 2_2/Hash.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> hSet = new HashSet<int>();
+            int x;
+            while ((x = int.Parse(Console.ReadLine())) != 0)
+            {
+                int tmp = 100 - x;
+                if (!hSet.Contains(tmp))
+                {
+                    hSet.Add(x);
+                }
+                else
+                {
+                    Console.WriteLine("Yes");
+                    return;
+                }
+            }
+            Console.WriteLine("No");
+        }
+    }
+}
